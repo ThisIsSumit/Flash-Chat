e25599226eab76b243713ccd542f747be5b4e495
ThisIsSumit <sumitkumar453827@gmail.com>
2024-06-06 12:38:18 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp()
+      : await Firebase.initializeApp();
+  runApp(const FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+      routes: {
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+      },
+    );
+  }
+}
